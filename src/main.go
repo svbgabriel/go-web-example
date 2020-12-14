441ba5f711bf0c7a044f84c21e5ec4d9098df0de
Gabriel Batista <svbgabriel@gmail.com>
2020-12-13 21:06:40 -0300

    Adiciona exemplo de conexão ao banco de dados

--- src/main.go
+++ src/main.go
@@ -1,32 +1,49 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"net/http"
+    "database/sql"
+    "fmt"
+    "html/template"
+    "net/http"
+
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type Post struct {
-	Id int
-	Title string
-	Body string
+    Id    int
+    Title string
+    Body  string
 }
+
+var db, err = sql.Open("mysql", "go:go@/go-web?charset=utf8")
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    smtm, err := db.Prepare("INSERT INTO posts(title, body) values(?,?)")
+    checkErr(err)
 
-		post := Post{Id: 1, Title: "Unnamed post", Body: "No content"}
+    _, err = smtm.Exec("My First Post", "My First Content")
+    checkErr(err)
 
-		if title := r.FormValue("title"); title != "" {
-			post.Title = title
-		}
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		t := template.Must(template.ParseFiles("templates/index.html"))
-		if err := t.ExecuteTemplate(w, "index.html", post); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+        post := Post{Id: 1, Title: "Unnamed post", Body: "No content"}
 
-	fmt.Println(http.ListenAndServe(":8080", nil))
+        if title := r.FormValue("title"); title != "" {
+            post.Title = title
+        }
+
+        t := template.Must(template.ParseFiles("templates/index.html"))
+        if err := t.ExecuteTemplate(w, "index.html", post); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+    })
+
+    fmt.Println(http.ListenAndServe(":8080", nil))
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
+    }
 }
