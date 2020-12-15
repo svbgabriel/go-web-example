620c7adbd256f8b3b7f5619753f87b971084a429
Gabriel Batista <svbgabriel@gmail.com>
2020-12-15 19:44:59 -0300

    Usa os resultados do banco no template

--- src/main.go
+++ src/main.go
@@ -1,54 +1,50 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "html/template"
-    "net/http"
+	"database/sql"
+	"fmt"
+	"html/template"
+	"net/http"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Post struct {
-    Id    int
-    Title string
-    Body  string
+	Id    int
+	Title string
+	Body  string
 }
 
 var db, err = sql.Open("mysql", "go:go@/go-web?charset=utf8")
 
 func main() {
 
-    rows, err := db.Query("SELECT * FROM posts")
-    checkErr(err)
+	rows, err := db.Query("SELECT * FROM posts")
+	checkErr(err)
 
-    var items []Post
+	var items []Post
 
-    for rows.Next() {
-        post := Post{}
-        rows.Scan(&post.Id, &post.Title, &post.Body)
-        items = append(items, post)
-    }
+	for rows.Next() {
+		post := Post{}
+		rows.Scan(&post.Id, &post.Title, &post.Body)
+		items = append(items, post)
+	}
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	db.Close()
 
-        post := Post{Id: 1, Title: "Unnamed post", Body: "No content"}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-        if title := r.FormValue("title"); title != "" {
-            post.Title = title
-        }
+		t := template.Must(template.ParseFiles("templates/index.html"))
+		if err := t.ExecuteTemplate(w, "index.html", items); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 
-        t := template.Must(template.ParseFiles("templates/index.html"))
-        if err := t.ExecuteTemplate(w, "index.html", post); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-    })
-
-    fmt.Println(http.ListenAndServe(":8080", nil))
+	fmt.Println(http.ListenAndServe(":8080", nil))
 }
 
 func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
