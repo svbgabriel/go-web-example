4e5cca5881d20ada245e15b1dc054b0f8a215b8c
Gabriel Batista <svbgabriel@gmail.com>
2020-12-18 19:28:08 -0300

    Recupera Post especifico do banco

--- src/main.go
+++ src/main.go
@@ -22,6 +22,8 @@ func main() {
 
 	r := mux.NewRouter()
 	r.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("static/"))))
+
+	r.HandleFunc("/{id}/view", ViewHandler)
 	r.HandleFunc("/", HomeHandler)
 
 	fmt.Println(http.ListenAndServe(":8080", r))
@@ -43,10 +45,25 @@ func ListPosts() []Post {
 	return items
 }
 
+func GetPostById(id string) Post {
+	row := db.QueryRow("SELECT * FROM posts where id=?", id)
+	post := Post{}
+	row.Scan(&post.Id, &post.Title, &post.Body)
+	return post
+}
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	t := template.Must(template.ParseFiles("templates/index.html"))
 	if err := t.ExecuteTemplate(w, "index.html", ListPosts()); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func ViewHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	t := template.Must(template.ParseFiles("templates/view.html"))
+	if err := t.ExecuteTemplate(w, "view.html", GetPostById(id)); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
